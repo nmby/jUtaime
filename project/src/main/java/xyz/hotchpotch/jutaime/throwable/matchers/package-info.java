01f2ca32c2c606c6137fadfc010813ddb13ce77c
nmby <nmby@hotchpotch.xyz>
2015-07-26 14:49:08 +0900

    javadoc仮生成

--- project/src/main/java/xyz/hotchpotch/jutaime/throwable/matchers/package-info.java
+++ project/src/main/java/xyz/hotchpotch/jutaime/throwable/matchers/package-info.java
@@ -1,6 +1,7 @@
 /**
  * 例外に関する検査を行うための各種 {@code Matcher} を提供します。<br>
- * 本パッケージに含まれるクラスを直接利用するのではなく、{@link RaiseMatchers} が提供する static ファクトリを利用するのが便利です。<br>
+ * 本パッケージに含まれるクラスを直接利用するのではなく、{@link xyz.hotchpotch.jutaime.throwable.RaiseMatchers RaiseMatchers}
+ * が提供する static ファクトリを利用するのが便利です。<br>
  * 利用方法の概要は {@link xyz.hotchpotch.jutaime.throwable} パッケージの説明を参照してください。<br>
  * 
  * @author nmby
