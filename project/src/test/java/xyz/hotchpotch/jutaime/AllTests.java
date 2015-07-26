730d90d2b49993314145be8e0416e8a11d8aad70
nmby <nmby@hotchpotch.xyz>
2015-07-26 14:17:24 +0900

    テスト整備

---
+++ project/src/test/java/xyz/hotchpotch/jutaime/AllTests.java
@@ -0,0 +1,43 @@
+package xyz.hotchpotch.jutaime;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import xyz.hotchpotch.jutaime.throwable.RaiseMatcherTest;
+import xyz.hotchpotch.jutaime.throwable.RaiseMatchersTest;
+import xyz.hotchpotch.jutaime.throwable.TesteeTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.InChainBaseTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.InChainExactTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.InChainTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.NoCauseTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.RaiseBaseTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.RaiseExactTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.RaiseNothingTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.RaiseTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.RootCauseBaseTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.RootCauseExactTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.RootCauseTest;
+import xyz.hotchpotch.jutaime.throwable.matchers.ThrowableBaseMatcherTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    RaiseMatchersTest.class,
+    RaiseMatcherTest.class,
+    TesteeTest.class,
+    
+    InChainBaseTest.class,
+    InChainExactTest.class,
+    InChainTest.class,
+    NoCauseTest.class,
+    RaiseBaseTest.class,
+    RaiseExactTest.class,
+    RaiseNothingTest.class,
+    RaiseTest.class,
+    RootCauseBaseTest.class,
+    RootCauseExactTest.class,
+    RootCauseTest.class,
+    ThrowableBaseMatcherTest.class
+})
+public class AllTests {
+}
--- project/src/test/java/xyz/hotchpotch/jutaime/throwable/RaiseMatchersTest.java
+++ project/src/test/java/xyz/hotchpotch/jutaime/throwable/RaiseMatchersTest.java
@@ -3,133 +3,182 @@ package xyz.hotchpotch.jutaime.throwable;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.io.IOError;
+import java.io.IOException;
+
 import org.junit.Test;
 
-import xyz.hotchpotch.jutaime.throwable.matchers.InChain;
-import xyz.hotchpotch.jutaime.throwable.matchers.InChainExact;
-import xyz.hotchpotch.jutaime.throwable.matchers.NoCause;
-import xyz.hotchpotch.jutaime.throwable.matchers.Raise;
-import xyz.hotchpotch.jutaime.throwable.matchers.RaiseExact;
 import xyz.hotchpotch.jutaime.throwable.matchers.RaiseNothing;
-import xyz.hotchpotch.jutaime.throwable.matchers.RootCause;
-import xyz.hotchpotch.jutaime.throwable.matchers.RootCauseExact;
 
 public class RaiseMatchersTest {
     
     @Test
     public void testRaiseClassOfQextendsThrowable() {
         assertThat(RaiseMatchers.raise(Exception.class), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.raise(Exception.class).toString(),
-                is(Raise.raise(Exception.class).toString()));
+        
+        assertThat(Testee.of(() -> { throw new IOException(new Throwable()); }),
+                RaiseMatchers.raise(Exception.class));
+        assertThat(Testee.of(() -> { throw new IOError(new Throwable()); }),
+                not(RaiseMatchers.raise(Exception.class)));
     }
     
     @Test
     public void testRaiseExactClassOfQextendsThrowable() {
         assertThat(RaiseMatchers.raiseExact(Exception.class), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.raiseExact(Exception.class).toString(),
-                is(RaiseExact.raiseExact(Exception.class).toString()));
+        
+        assertThat(Testee.of(() -> { throw new Exception("msg"); }),
+                RaiseMatchers.raiseExact(Exception.class));
+        assertThat(Testee.of(() -> { throw new IOException("msg"); }),
+                not(RaiseMatchers.raiseExact(Exception.class)));
     }
     
     @Test
     public void testRaiseClassOfQextendsThrowableString() {
         assertThat(RaiseMatchers.raise(Exception.class, "test msg"), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.raise(Exception.class, "test msg").toString(),
-                is(Raise.raise(Exception.class, "test msg").toString()));
+        
+        assertThat(Testee.of(() -> { throw new IOException("test msg"); }),
+                RaiseMatchers.raise(Exception.class, "test msg"));
+        assertThat(Testee.of(() -> { throw new IOException("diff msg"); }),
+                not(RaiseMatchers.raise(Exception.class, "test msg")));
     }
     
     @Test
     public void testRaiseExactClassOfQextendsThrowableString() {
         assertThat(RaiseMatchers.raiseExact(Exception.class, "test msg"), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.raiseExact(Exception.class, "test msg").toString(),
-                is(RaiseExact.raiseExact(Exception.class, "test msg").toString()));
+        
+        assertThat(Testee.of(() -> { throw new Exception("test msg"); }),
+                RaiseMatchers.raiseExact(Exception.class, "test msg"));
+        assertThat(Testee.of(() -> { throw new IOException("test msg"); }),
+                not(RaiseMatchers.raiseExact(Exception.class, "test msg")));
     }
     
     @Test
     public void testRaiseMatcherOfThrowable() {
         assertThat(RaiseMatchers.raise(instanceOf(Exception.class)), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.raise(instanceOf(Exception.class)).toString(),
-                is(Raise.raise(instanceOf(Exception.class)).toString()));
+        
+        assertThat(Testee.of(() -> { throw new IOException(new Throwable()); }),
+                RaiseMatchers.raise(instanceOf(Exception.class)));
+        assertThat(Testee.of(() -> { throw new IOError(new Throwable()); }),
+                not(RaiseMatchers.raise(instanceOf(Exception.class))));
     }
     
     @Test
     public void testRaiseNothing() {
         assertThat(RaiseMatchers.raiseNothing(), instanceOf(RaiseNothing.class));
+        
+        assertThat(Testee.of(() -> {}),
+                RaiseMatchers.raiseNothing());
+        assertThat(Testee.of(() -> { throw new Exception(); }),
+                not(RaiseMatchers.raiseNothing()));
     }
     
     @Test
     public void testNoCause() {
         assertThat(RaiseMatchers.noCause(), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.noCause().toString(),
-                is(NoCause.noCause().toString()));
+        
+        assertThat(Testee.of(() -> { throw new Exception(); }),
+                RaiseMatchers.noCause());
+        assertThat(Testee.of(() -> { throw new Exception(new Throwable()); }),
+                not(RaiseMatchers.noCause()));
     }
     
     @Test
     public void testRootCauseClassOfQextendsThrowable() {
         assertThat(RaiseMatchers.rootCause(Exception.class), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.rootCause(Exception.class).toString(),
-                is(RootCause.rootCause(Exception.class).toString()));
+        
+        assertThat(Testee.of(() -> { throw new Throwable(new RuntimeException()); }),
+                RaiseMatchers.rootCause(Exception.class));
+        assertThat(Testee.of(() -> { throw new Throwable(new Throwable()); }),
+                not(RaiseMatchers.rootCause(Exception.class)));
     }
     
     @Test
     public void testRootCauseExactClassOfQextendsThrowable() {
         assertThat(RaiseMatchers.rootCauseExact(Exception.class), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.rootCauseExact(Exception.class).toString(),
-                is(RootCauseExact.rootCauseExact(Exception.class).toString()));
+        
+        assertThat(Testee.of(() -> { throw new Throwable(new Exception()); }),
+                RaiseMatchers.rootCauseExact(Exception.class));
+        assertThat(Testee.of(() -> { throw new Throwable(new IOException()); }),
+                not(RaiseMatchers.rootCauseExact(Exception.class)));
     }
     
     @Test
     public void testRootCauseClassOfQextendsThrowableString() {
         assertThat(RaiseMatchers.rootCause(Exception.class, "test msg"), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.rootCause(Exception.class, "test msg").toString(),
-                is(RootCause.rootCause(Exception.class, "test msg").toString()));
+        
+        assertThat(Testee.of(() -> { throw new Throwable(new IOException("test msg")); }),
+                RaiseMatchers.rootCause(Exception.class, "test msg"));
+        assertThat(Testee.of(() -> { throw new Throwable(new IOException("diff msg")); }),
+                not(RaiseMatchers.rootCause(Exception.class, "test msg")));
     }
     
     @Test
     public void testRootCauseExactClassOfQextendsThrowableString() {
         assertThat(RaiseMatchers.rootCauseExact(Exception.class, "test msg"), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.rootCauseExact(Exception.class, "test msg").toString(),
-                is(RootCauseExact.rootCauseExact(Exception.class, "test msg").toString()));
+        
+        assertThat(Testee.of(() -> { throw new Throwable(new Exception("test msg")); }),
+                RaiseMatchers.rootCauseExact(Exception.class, "test msg"));
+        assertThat(Testee.of(() -> { throw new Throwable(new IOException("test msg")); }),
+                not(RaiseMatchers.rootCauseExact(Exception.class, "test msg")));
     }
     
     @Test
     public void testRootCauseMatcherOfThrowable() {
         assertThat(RaiseMatchers.rootCause(instanceOf(Exception.class)), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.rootCause(instanceOf(Exception.class)).toString(),
-                is(RootCause.rootCause(instanceOf(Exception.class)).toString()));
+        
+        assertThat(Testee.of(() -> { throw new Throwable(new RuntimeException()); }),
+                RaiseMatchers.rootCause(instanceOf(Exception.class)));
+        assertThat(Testee.of(() -> { throw new Throwable(new RuntimeException(new Error())); }),
+                not(RaiseMatchers.rootCause(instanceOf(Exception.class))));
     }
     
     @Test
     public void testInChainClassOfQextendsThrowable() {
         assertThat(RaiseMatchers.inChain(Exception.class), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.inChain(Exception.class).toString(),
-                is(InChain.inChain(Exception.class).toString()));
+        
+        assertThat(Testee.of(() -> { throw new Error(new IOException(new Error())); }),
+                RaiseMatchers.inChain(Exception.class));
+        assertThat(Testee.of(() -> { throw new Error(new IOError(new Error())); }),
+                not(RaiseMatchers.inChain(Exception.class)));
     }
     
     @Test
     public void testInChainExactClassOfQextendsThrowable() {
         assertThat(RaiseMatchers.inChainExact(Exception.class), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.inChainExact(Exception.class).toString(),
-                is(InChainExact.inChainExact(Exception.class).toString()));
+        
+        assertThat(Testee.of(() -> { throw new Error(new Exception(new Error())); }),
+                RaiseMatchers.inChainExact(Exception.class));
+        assertThat(Testee.of(() -> { throw new Error(new IOException(new Error())); }),
+                not(RaiseMatchers.inChainExact(Exception.class)));
     }
     
     @Test
     public void testInChainClassOfQextendsThrowableString() {
         assertThat(RaiseMatchers.inChain(Exception.class, "test msg"), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.inChain(Exception.class, "test msg").toString(),
-                is(InChain.inChain(Exception.class, "test msg").toString()));
+        
+        assertThat(Testee.of(() -> { throw new Error(new IOException("test msg", new Error())); }),
+                RaiseMatchers.inChain(Exception.class, "test msg"));
+        assertThat(Testee.of(() -> { throw new Error(new IOException("diff msg", new Error())); }),
+                not(RaiseMatchers.inChain(Exception.class, "test msg")));
     }
     
     @Test
     public void testInChainExactClassOfQextendsThrowableString() {
         assertThat(RaiseMatchers.inChainExact(Exception.class, "test msg"), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.inChainExact(Exception.class, "test msg").toString(),
-                is(InChainExact.inChainExact(Exception.class, "test msg").toString()));
+        
+        assertThat(Testee.of(() -> { throw new Error(new Exception("test msg", new Error())); }),
+                RaiseMatchers.inChainExact(Exception.class, "test msg"));
+        assertThat(Testee.of(() -> { throw new Error(new IOException("test msg", new Error())); }),
+                not(RaiseMatchers.inChainExact(Exception.class, "test msg")));
     }
     
     @Test
     public void testInChainMatcherOfThrowable() {
         assertThat(RaiseMatchers.inChain(instanceOf(Exception.class)), instanceOf(RaiseMatcher.class));
-        assertThat(RaiseMatchers.inChain(instanceOf(Exception.class)).toString(),
-                is(InChain.inChain(instanceOf(Exception.class)).toString()));
+        
+        assertThat(Testee.of(() -> { throw new Error(new IOException(new Error())); }),
+                RaiseMatchers.inChain(instanceOf(Exception.class)));
+        assertThat(Testee.of(() -> { throw new Error(new IOError(new Error())); }),
+                not(RaiseMatchers.inChain(instanceOf(Exception.class))));
     }
 }
