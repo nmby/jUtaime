13093f4c04d2a84f5bfaf94bb244312cbc905e1b
nmby <nmby@hotchpotch.xyz>
2015-09-03 21:57:54 +0900

    javadoc 記述

--- project/src/main/java/xyz/hotchpotch/jutaime/serializable/experimental/FailToDeserializeException.java
+++ project/src/main/java/xyz/hotchpotch/jutaime/serializable/experimental/FailToDeserializeException.java
@@ -1,5 +1,12 @@
 package xyz.hotchpotch.jutaime.serializable.experimental;
 
+/**
+ * デシリアル化に失敗したことを表す実行時例外です。<br>
+ * 失敗の原因となった例外を cause として保持します。<br>
+ * 
+ * @since 
+ * @author nmby
+ */
 public class FailToDeserializeException extends RuntimeException {
     
     // ++++++++++++++++ static members ++++++++++++++++
