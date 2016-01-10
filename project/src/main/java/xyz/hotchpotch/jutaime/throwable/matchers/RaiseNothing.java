63cff3f2d719139daea68bdc629a6b98c2a160d7
nmby <nmby@hotchpotch.xyz>
2016-01-10 22:59:25 +0900

    raiseNothing(T), raiseNothing(Matcher<T>) を追加

--- project/src/main/java/xyz/hotchpotch/jutaime/throwable/matchers/RaiseNothing.java
+++ project/src/main/java/xyz/hotchpotch/jutaime/throwable/matchers/RaiseNothing.java
@@ -1,5 +1,9 @@
 package xyz.hotchpotch.jutaime.throwable.matchers;
 
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.Objects;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -31,9 +35,56 @@ public class RaiseNothing<T> extends TypeSafeMatcher<Testee<T>> {
         return new RaiseNothing();
     }
     
+    /**
+     * 検査対象のオペレーションが例外やエラーをスローせずに正常終了し、戻り値が期待通りであることを検査する {@code Matcher} オブジェクトを返します。<br>
+     * 
+     * @param <T> 検査対象のオペレーションの戻り値の型
+     * @param matcher オペレーションの戻り値を検査する {@code Matcher}
+     * @return 検査対象のオペレーションが正常終了し、戻り値が期待通りであることを検査する {@code Matcher}
+     * @throws NullPointerException {@code matcher} が {@code null} の場合
+     * @since 1.4.0
+     */
+    public static <T> Matcher<Testee<T>> raiseNothing(Matcher<? super T> matcher) {
+        Objects.requireNonNull(matcher);
+        return new RaiseNothing<>(matcher);
+    }
+    
+    /**
+     * 検査対象のオペレーションが例外やエラーをスローせずに正常終了し、戻り値が期待通りであることを検査する {@code Matcher} オブジェクトを返します。<br>
+     * 次の2つの呼び出しは同値です。
+     * <pre>
+     *     raiseNothing(expectedValue);
+     *     raiseNothing(org.hamcrest.CoreMatchers.is(expectedValue));
+     * </pre>
+     * なお、{@code expectedValue} に {@code null} を指定することはできません。<br>
+     * 検査対象のオペレーションの戻り値として {@code null} が期待される場合は、次のコードで検証してください。<br>
+     * <pre>
+     *     raiseNothing(org.hamcrest.CoreMatchers.nullValue());
+     * </pre>
+     * 
+     * @param <T> 検査対象のオペレーションの戻り値の型
+     * @param expectedValue 期待される戻り値
+     * @return 検査対象のオペレーションが正常終了し、戻り値が期待通りであることを検査する {@code Matcher}
+     * @throws NullPointerException {@code expectedValue} が {@code null} の場合
+     * @see org.hamcrest.CoreMatchers#is(Object)
+     * @see org.hamcrest.CoreMatchers#nullValue()
+     * @since 1.4.0
+     */
+    public static <T> Matcher<Testee<T>> raiseNothing(T expectedValue) {
+        Objects.requireNonNull(expectedValue);
+        return new RaiseNothing<>(is(expectedValue));
+    }
+    
     // [instance members] ++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
+    private final Matcher<? super T> matcher;
+    
     private RaiseNothing() {
+        matcher = null;
+    }
+    
+    private RaiseNothing(Matcher<? super T> matcher) {
+        this.matcher = matcher;
     }
     
     /**
@@ -43,8 +94,8 @@ public class RaiseNothing<T> extends TypeSafeMatcher<Testee<T>> {
     protected boolean matchesSafely(Testee<T> testee) {
         assert testee != null;
         try {
-            testee.call();
-            return true;
+            T result = testee.call();
+            return matcher == null || matcher.matches(result);
         } catch (Throwable t) {
             return false;
         }
@@ -56,7 +107,10 @@ public class RaiseNothing<T> extends TypeSafeMatcher<Testee<T>> {
     @Override
     public void describeTo(Description description) {
         if (description != null) {
-            description.appendText("throw nothing");
+            description.appendText("throws nothing");
+            if (matcher != null) {
+                description.appendText(String.format(" <%s>", matcher.toString()));
+            }
         }
     }
 }
--- project/src/test/java/xyz/hotchpotch/jutaime/throwable/matchers/RaiseNothingTest.java
+++ project/src/test/java/xyz/hotchpotch/jutaime/throwable/matchers/RaiseNothingTest.java
@@ -3,6 +3,7 @@ package xyz.hotchpotch.jutaime.throwable.matchers;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import xyz.hotchpotch.jutaime.throwable.Testee;
@@ -14,13 +15,29 @@ public class RaiseNothingTest {
     // [instance members] ++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
     @Test
-    public void testRaiseNothing() {
+    public void testRaiseNothing1() {
         // インスタンス化の検査
         assertThat(RaiseNothing.raiseNothing(), instanceOf(RaiseNothing.class));
     }
     
     @Test
-    public void testMatchesSafelyTestee() {
+    public void testRaiseNothing2() {
+        // インスタンス化の検査
+        assertThat(RaiseNothing.raiseNothing(not("A")), instanceOf(RaiseNothing.class));
+        assertThat(RaiseNothing.raiseNothing(nullValue()), instanceOf(RaiseNothing.class));
+        assertThat(Testee.of(() -> RaiseNothing.raiseNothing((Matcher<?>) null)), Raise.raise(NullPointerException.class));
+    }
+    
+    @Test
+    public void testRaiseNothing3() {
+        // インスタンス化の検査
+        assertThat(RaiseNothing.raiseNothing("A"), instanceOf(RaiseNothing.class));
+        assertThat(RaiseNothing.raiseNothing(123), instanceOf(RaiseNothing.class));
+        assertThat(Testee.of(() -> RaiseNothing.raiseNothing(null)), Raise.raise(NullPointerException.class));
+    }
+    
+    @Test
+    public void testMatchesSafelyTestee1() {
         // 正常終了時は合格と判定する。
         assertThat(Testee.of(() -> {}), RaiseNothing.raiseNothing());
         assertThat(Testee.of(() -> null), RaiseNothing.raiseNothing());
@@ -35,8 +52,27 @@ public class RaiseNothingTest {
     }
     
     @Test
-    public void testDescribeTo() {
-        assertThat(RaiseNothing.raiseNothing().toString(), is("throw nothing"));
+    public void testMatchesSafelyTestee2() {
+        // 正常終了時かつ戻り値が期待通りの場合に合格と判定する。
+        assertThat(Testee.of(() -> null), RaiseNothing.raiseNothing(nullValue()));
+        assertThat(Testee.of(() -> null), not(RaiseNothing.raiseNothing(123)));
+        assertThat(Testee.of(() -> 1 + 2), RaiseNothing.raiseNothing(3));
+        assertThat(Testee.of(() -> 1 + 2), not(RaiseNothing.raiseNothing(4)));
+        assertThat(Testee.of(() -> new Exception("not thrown")), RaiseNothing.raiseNothing(instanceOf(Exception.class)));
+        assertThat(Testee.of(() -> new Exception("not thrown")), not(RaiseNothing.raiseNothing(instanceOf(Error.class))));
+        
+        // 例外発生時は不合格と判定する。
+        assertThat(Testee.of(() -> { throw new Throwable(); }), not(RaiseNothing.raiseNothing(123)));
+        assertThat(Testee.of(() -> { throw new Error(); }), not(RaiseNothing.raiseNothing("abc")));
+        assertThat(Testee.of(() -> { throw new Exception(); }), not(RaiseNothing.raiseNothing(nullValue())));
+        assertThat(Testee.of(() -> { throw new RuntimeException(); }), not(RaiseNothing.raiseNothing(is(true))));
+        
+        assertThat(Testee.of(() -> RaiseNothing.raiseNothing(null)), Raise.raise(NullPointerException.class));
+    }
+    
+    @Test
+    public void testDescribeTo1() {
+        assertThat(RaiseNothing.raiseNothing().toString(), is("throws nothing"));
         
         try {
             // null を与えても安全に終了する。
@@ -44,5 +80,13 @@ public class RaiseNothingTest {
         } catch (NullPointerException e) {
             fail();
         }
+    }
+    
+    @Test
+    public void testDescribeTo2() {
+        assertThat(RaiseNothing.raiseNothing("A").toString(), is("throws nothing <is \"A\">"));
+        assertThat(RaiseNothing.raiseNothing(123).toString(), is("throws nothing <is <123>>"));
+        assertThat(RaiseNothing.raiseNothing(not("A")).toString(), is("throws nothing <not \"A\">"));
+        assertThat(RaiseNothing.raiseNothing(nullValue()).toString(), is("throws nothing <null>"));
     }
 }
